52adb72d2b58cfd88b65fb1963d851433d92a1c2
FYClamityDestiny <99613329+FYClamityDestiny@users.noreply.github.com>
2023-01-24 18:38:59 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var first_name = "Farhan";
+var last_name = " Yasim"
+var full_name = first_name + last_name;
+var numerator_1 = 20;
+var numerator_2 = 10;
+var division = numerator_1 / numerator_2;
+var multiplication = numerator_1 * numerator_2;
+var addition =  numerator_1 + numerator_2;
+var subtraction = numerator_1 - numerator_2;
+console.log("My first console project!");
+console.log(full_name);
+console.log(division);
+console.log(multiplication);
+console.log(addition);
+console.log(subtraction)
