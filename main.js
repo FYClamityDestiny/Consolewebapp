0e6004d430d9beca11c43d4e43c893c095158f85
FYClamityDestiny <99613329+FYClamityDestiny@users.noreply.github.com>
2023-01-24 19:01:23 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var first_name = "Farhan";
+var last_name = " Yasim"
+var full_name = first_name + last_name;
+var numerator_1 = 20;
+var numerator_2 = 10;
+var division = numerator_1 / numerator_2;
+var multiplication = numerator_1 * numerator_2;
+var addition =  numerator_1 + numerator_2;
+var subtraction = numerator_1 - numerator_2;
+console.log("My first console project!");
+console.log(full_name);
+console.log(division);
+console.log(multiplication);
+console.log(addition);
+console.log(subtraction);
+console.log("Number 1:" + numerator_1);
+console.log("Number 2:" + numerator_2);
+console.log("We are doing mathemathical operations in this order:DIVISION, MULTIPLICATION, ADDITION and SUBTRACTION!");
